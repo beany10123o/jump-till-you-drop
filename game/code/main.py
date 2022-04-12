ecd495a9e91bc8621c2abbf4d73083b8fd08b67f
Tim <jamespsmith742@gmail.com>
2022-04-12 20:11:12 +0100

    Made the game actually draw Draa

--- game/code/main.py
+++ game/code/main.py
@@ -1,9 +1,31 @@
-import pygame
+import sys, pygame, os #Import all needed modules
 
-pygame.display.set_caption('hello')
+#This is the "game" folder to get files from
+working_path = os.path.dirname(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-screen=pygame.display.set_mode((100, 100))
+#Set window details
+window_name = 'hello'
+window_size = (500, 500) #width, height
 
+#Setup sprites
+draa_jumping = pygame.image.load(os.path.join(working_path, "images", "sprites", "Draa", "jumping.png"))
+
+#Initialise Window
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption(window_name)
+
+#Game loop
 while True:
+    #Make the close button work
+    #This also stops the program from making the cursor go into laggy mode
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    
+    #Draw the background
     screen.fill((0, 255, 0))
+
+    #Draw Draa
+    screen.blit(draa_jumping, (0, 0))
+
+    #Put everything we have draw actually on the screen
     pygame.display.flip()
